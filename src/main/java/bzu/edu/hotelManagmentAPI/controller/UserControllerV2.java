e2b29ba87d637b6480a20b47fc1c7f03dad8ba2e
Belal Hmeidat <belalhmeidat@icloud.com>
2024-06-07 15:47:17 +0300

    commented controller

--- src/main/java/bzu/edu/hotelManagmentAPI/controller/UserControllerV2.java
+++ src/main/java/bzu/edu/hotelManagmentAPI/controller/UserControllerV2.java
@@ -1,23 +1,23 @@
-package bzu.edu.hotelManagmentAPI.controller;
+// package bzu.edu.hotelManagmentAPI.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import bzu.edu.hotelManagmentAPI.service.UserService;
+// import bzu.edu.hotelManagmentAPI.service.UserService;
 
-@RestController
-@RequestMapping("/api/v2/users")
-public class UserControllerV2 extends UserController{
+// @RestController
+// @RequestMapping("/api/v2/users")
+// public class UserControllerV2 extends UserController{
 
-    private final UserService userService;
+//     private final UserService userService;
     
     
-    @Autowired
-    public UserControllerV2(UserService userService) {
-        super(userService);
-        this.userService = userService;
-    }
+//     @Autowired
+//     public UserControllerV2(UserService userService) {
+//         super(userService);
+//         this.userService = userService;
+//     }
 
     
-}
+// }
